42469140efcd97334a021d12a821bf0ea5776818
Michele <michele.tessari12@gmail.com>
2018-08-21 11:13:09 +0200

    QUERY FINITE!!!!!!!!!!
    non é vero in futuro se ne aggiungeranno altre :c

--- src/main/java/it/unitn/webprog2018/ueb/shoppinglist/dao/mysql/ProductsCategoryDAOImpl.java
+++ src/main/java/it/unitn/webprog2018/ueb/shoppinglist/dao/mysql/ProductsCategoryDAOImpl.java
@@ -1,7 +1,7 @@
 package it.unitn.webprog2018.ueb.shoppinglist.dao.mysql;
 
 import it.unitn.webprog2018.ueb.shoppinglist.dao.DAOFactory;
-import it.unitn.webprog2018.ueb.shoppinglist.dao.exceptions.DaoException;
+import it.unitn.webprog2018.ueb.shoppinglist.dao.exceptions.*;
 import it.unitn.webprog2018.ueb.shoppinglist.dao.interfaces.ProductsCategoryDAO;
 import it.unitn.webprog2018.ueb.shoppinglist.entities.ProductsCategory;
 import java.sql.Connection;
@@ -11,6 +11,8 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -22,12 +24,6 @@ public class ProductsCategoryDAOImpl extends AbstractDAO implements ProductsCate
 	public ProductsCategoryDAOImpl(Connection connection, DAOFactory dAOFactory) {
 		super(connection, dAOFactory);
 	}
-	
-	/**
-	 * @param mathing, parola chiave;
-	 * @return un'ArrayList con tutte le categorie di prodotti con nomi conteneti quella parola, lancia una DaoException per qualsiasi errore riscontrato
-	 * @throws DaoException
-	 */
 	
 	@Override
 	public List<ProductsCategory> getFromQuery(String matching) throws DaoException{
@@ -55,48 +51,123 @@ public class ProductsCategoryDAOImpl extends AbstractDAO implements ProductsCate
 			
 			return list;
 		}
-		catch(SQLException e){
-			throw new DaoException(e);
+		catch(SQLException ex){
+			Logger.getLogger(UserDAOimpl.class.getName()).log(Level.SEVERE, null, ex);
+			throw new DaoException(ex);
 		}
 	}
-	
-	/**
-	 * @param pc, prodotto da aggiungere
-	 * @return true operazione andata a buon termine, false altrimenti
-	 * @throws DaoException 
-	 */
 	
 	@Override
 	public Boolean addProductsCategory(ProductsCategory pc) throws DaoException{
-		try{
-			String query = "INSERT INTO productscategories (name,category,description,logo) VALUES (\""+
-					pc.getName()+ "\"," +
-					(pc.getCategory() < 0 ? "null" : pc.getCategory())+ ",\"" +
-					pc.getDescription()+ "\",\"" +
-					pc.getLogo()+"\")";
-			PreparedStatement st = this.getCon().prepareStatement(query);
-			int count = st.executeUpdate();
-			st.close();
-			return (count == 1);
+		Boolean valid = pc.isVaildOnCreate(dAOFactory);
+		if(valid){
+			try{
+				String query = "INSERT INTO productscategories (name,category,description,logo) VALUES (\""+
+						pc.getName()+ "\"," +
+						(pc.getCategory() < 0 ? "null" : pc.getCategory())+ ",\"" +
+						pc.getDescription()+ "\",\"" +
+						pc.getLogo()+"\")";
+				PreparedStatement st = this.getCon().prepareStatement(query);
+				st.executeUpdate();
+				st.close();
+				return valid;
+			}
+			catch(SQLException ex){
+				Logger.getLogger(UserDAOimpl.class.getName()).log(Level.SEVERE, null, ex);
+				throw new DaoException(ex);
+			}
 		}
-		catch(SQLException e){
-			throw new DaoException(e);
-		}
-	}
-
-	@Override
-	public List<ProductsCategory> getAll() throws DaoException{
-		throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-	}
-
-	@Override
-	public ProductsCategory getById(Integer id) throws DaoException {
-		throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-	}
-
-	@Override
-	public ProductsCategory getByName(String name) throws DaoException {
-		throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+		return valid;
 	}
 	
+	@Override
+	public List<ProductsCategory> getAll() throws DaoException{
+		List<ProductsCategory> list = new ArrayList<>();
+		try{
+			String query =	"SELECT id,name,category,description,logo " +
+					"FROM productscategories";
+			Statement st = this.getCon().createStatement();
+			ResultSet rs = st.executeQuery(query);
+			ProductsCategory pc;
+			int i;
+			while(rs.next())
+			{
+				i = 1;
+				pc = new ProductsCategory();
+				pc.setId(rs.getInt(i++));
+				pc.setName(rs.getString(i++));
+				pc.setCategory(rs.getInt(i++));
+				pc.setDescription(rs.getString(i++));
+				pc.setLogo(rs.getString(i++));
+				list.add(pc);
+			}
+			rs.close();
+			st.close();
+			return list;
+		}
+		catch(SQLException ex){
+			Logger.getLogger(UserDAOimpl.class.getName()).log(Level.SEVERE, null, ex);
+			throw new DaoException(ex);
+		}
+	}
+	
+	@Override
+	public ProductsCategory getById(Integer id) throws DaoException {
+		try{
+			ProductsCategory pc = new ProductsCategory();
+			String query = "SELECT name,category,description,logo " +
+					"FROM productscategories " +
+					"WHERE id = "+id;
+			Statement st = this.getCon().createStatement();
+			ResultSet rs = st.executeQuery(query);
+			if(rs.first())
+			{
+				int i = 1;
+				pc.setId(id);
+				pc.setName(rs.getString(i++));
+				pc.setCategory(rs.getInt(i++));
+				pc.setDescription(rs.getString(i++));
+				pc.setLogo(rs.getString(i++));
+				
+				rs.close();
+				st.close();
+				return pc;
+			}
+			throw new RecordNotFoundDaoException("Product category with id: " + id + " not found");
+		}
+		catch(SQLException ex){
+			Logger.getLogger(UserDAOimpl.class.getName()).log(Level.SEVERE, null, ex);
+			throw new DaoException(ex);
+		}
+	}
+	
+	@Override
+	public ProductsCategory getByName(String name) throws DaoException {
+		try{
+			ProductsCategory pc = new ProductsCategory();
+			String query = "SELECT id,category,description,logo " +
+					"FROM productscategories " +
+					"WHERE name = "+name;
+			Statement st = this.getCon().createStatement();
+			ResultSet rs = st.executeQuery(query);
+			if(rs.first())
+			{
+				int i = 1;
+				pc.setId(rs.getInt(i++));
+				pc.setName(name);
+				pc.setCategory(rs.getInt(i++));
+				pc.setDescription(rs.getString(i++));
+				pc.setLogo(rs.getString(i++));
+				
+				rs.close();
+				st.close();
+				return pc;
+			}
+			throw new RecordNotFoundDaoException("Product Category with name: " + name + " not found");
+		}
+		catch(SQLException ex){
+			Logger.getLogger(UserDAOimpl.class.getName()).log(Level.SEVERE, null, ex);
+			throw new DaoException(ex);
+		}
+	}
 }
