09d44ce21b3d67d98fd8ab1c8c28181699623680
Michele <michele.tessari12@gmail.com>
2018-08-26 16:54:56 +0200

    fix

--- flyway/sql/V8__Add_notification.sql
+++ flyway/sql/V8__Add_notification.sql
@@ -4,7 +4,6 @@ CREATE TABLE if not exists productsnotification(
     idlist integer not null,
     idproduct integer,
     idpublicproduct integer,
-    text varchar(255),
     isread tinyint(1) default 0,
     foreign key(idproduct) references products(id),
     foreign key(idpublicproduct) references publicproducts(id),
@@ -23,46 +22,64 @@ ADD exp_average integer default -1,
 ADD add_count mediumint unsigned default 0;
 
 delimiter //
+create function timeThreshold() returns integer deterministic
+begin
+	return 60;
+end//
+
+create function countThreshold() returns integer deterministic
+begin
+	return 3;
+end//
+
 CREATE FUNCTION exp_average_func(_lastinsert datetime(1),_exp_average_prec integer) returns integer deterministic# minuti di intervallo di previsione per il prossimo inserimento
 BEGIN
 	set @alpha = 0.5;
 	set @minutes = abs(timestampdiff(MINUTE,now(1),_lastinsert));
     return if(_exp_average_prec < 0, @minutes , @minutes*@alpha + _exp_average_prec*(1-@alpha));
 END//
-delimiter ;
 
-delimiter //
 CREATE PROCEDURE addProductOnList(IN _idproduct integer,IN _idlist integer)
 BEGIN
 	if exists (select 1 from productsonlists where idproduct = _idproduct and idlist = _idlist limit 1)
     then
-		UPDATE productsonlists set quantity = 1,exp_average = exp_average_func(lastinsert,exp_average),lastinsert = now(1),add_count = add_count + 1 where idproduct = _idproduct and idlist = _idlist;
+		set @lastinsert = (select lastinsert from productsonlists where idproduct = _idproduct and idlist = _idlist);
+		UPDATE productsonlists set 	quantity = 1,
+									exp_average = @exp_average := exp_average_func(lastinsert,exp_average),
+                                    add_count = @add_count := if(abs(timestampdiff(MINUTE,now(1),lastinsert)) >= timeThreshold(),add_count + 1,add_count),
+                                    lastinsert = @now := now(1)
+		where idproduct = _idproduct and idlist = _idlist;
+		if abs(timestampdiff(MINUTE,@now,@lastinsert)) >= timeThreshold() and @add_count > countThreshold() then 
+			insert into productsnotification (time,idlist,idproduct,idpublicproduct) values (timestampadd(minute,@exp_average,@now),_idlist,_idproduct,null);
+		end if;
     else
 		INSERT INTO productsonlists (idproduct,idlist,quantity,add_count) VALUES (_idproduct,_idlist,1,1);
 	end if;
 END//
-delimiter ;
 
-delimiter //
 CREATE PROCEDURE addPublicProductOnList(IN _idproduct integer,IN _idlist integer)
 BEGIN
 	if exists (select 1 from publicproductsonlists where idpublicproduct = _idproduct and idlist = _idlist limit 1)
     then
-		UPDATE publicproductsonlists set quantity = 1,exp_average = exp_average_func(lastinsert,exp_average),lastinsert = now(1),add_count = add_count + 1 where idpublicproduct = _idproduct and idlist = _idlist;
+		set @lastinsert = (select lastinsert from publicproductsonlists where idpublicproduct = _idproduct and idlist = _idlist);
+		UPDATE publicproductsonlists set 	quantity = 1,
+											exp_average = @exp_average := exp_average_func(lastinsert,exp_average),
+                                            add_count = @add_count := if(abs(timestampdiff(MINUTE,now(1),lastinsert)) >= timeThreshold(),add_count + 1,add_count),
+                                            lastinsert = @now := now(1)
+		where idpublicproduct = _idproduct and idlist = _idlist;
+        if abs(timestampdiff(MINUTE,@now,@lastinsert)) >= timeThreshold() and @add_count > countThreshold() then 
+			insert into productsnotification (time,idlist,idproduct,idpublicproduct) values (timestampadd(minute,@exp_average,@now),_idlist,null,_idproduct);
+		end if;
     else
 		INSERT INTO publicproductsonlists (idpublicproduct,idlist,quantity,add_count) VALUES (_idproduct,_idlist,1,1);
 	end if;
 END//
-delimiter ;
 
-delimiter //
 CREATE PROCEDURE deleteProductOnList(IN _idproduct integer,IN _idlist integer)
 BEGIN
 	UPDATE productsonlists set quantity = -1 where idproduct = _idproduct and idlist = _idlist;
 END//
-delimiter ;
 
-delimiter //
 CREATE PROCEDURE deletePublicProductOnList(IN _idproduct integer,IN _idlist integer)
 BEGIN
 	UPDATE publicproductsonlists set quantity = -1 where idpublicproduct = _idproduct and idlist = _idlist;
