e0ad57d2d6a15712bdb02e4de87dd79e06e5c8f7
Michele <michele.tessari12@gmail.com>
2018-08-26 18:31:18 +0200

    iniziato NotificationDAOImpl

---
+++ src/main/java/it/unitn/webprog2018/ueb/shoppinglist/dao/mysql/NotificationDAOImpl.java
@@ -0,0 +1,110 @@
+/*
+* To change this license header, choose License Headers in Project Properties.
+* To change this template file, choose Tools | Templates
+* and open the template in the editor.
+*/
+package it.unitn.webprog2018.ueb.shoppinglist.dao.mysql;
+
+import it.unitn.webprog2018.ueb.shoppinglist.dao.DAOFactory;
+import it.unitn.webprog2018.ueb.shoppinglist.dao.exceptions.DaoException;
+import it.unitn.webprog2018.ueb.shoppinglist.dao.interfaces.NotificationDAO;
+import it.unitn.webprog2018.ueb.shoppinglist.entities.Notification;
+import it.unitn.webprog2018.ueb.shoppinglist.entities.Product;
+import it.unitn.webprog2018.ueb.shoppinglist.entities.PublicProduct;
+import it.unitn.webprog2018.ueb.shoppinglist.entities.User;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Michele
+ */
+public class NotificationDAOImpl extends AbstractDAO implements NotificationDAO{
+	
+	public NotificationDAOImpl(Connection con, DAOFactory dAOFactory) {
+		super(con, dAOFactory);
+	}
+	
+	@Override
+	public List<Notification> getNextNotifications(Timestamp nextRequest) throws DaoException {
+		List<Notification> list = new ArrayList<>();
+		try{
+			String query =	"select pn.id,pn.time,pn.idlist,l.name,l.iduser,u.name,pn.idproduct,p.name,pn.idpublicproduct,pp.name\n" +
+					"from productsnotification pn\n" +
+					"left join lists l on pn.idlist = l.id\n" +
+					"left join users u on l.iduser = u.id\n" +
+					"left join products p on pn.idproduct = p.id\n" +
+					"left join publicproducts pp on pn.idpublicproduct = pp.id\n" +
+					"where time <= now(1)";
+			Statement st = this.getCon().createStatement();
+			ResultSet rs = st.executeQuery(query);
+			Notification n;
+			it.unitn.webprog2018.ueb.shoppinglist.entities.List l;
+			User u;
+			Product p;
+			PublicProduct pp;
+			int i;
+			while(rs.next())
+			{
+				i = 1;
+				n = new Notification();
+				l = new it.unitn.webprog2018.ueb.shoppinglist.entities.List();
+				u = new User();
+				p = new Product();
+				pp = new PublicProduct();
+				
+				n.setId(rs.getInt(i++));
+				n.setTime(rs.getTimestamp(i++));
+				l.setId(rs.getInt(i++));
+				l.setName(rs.getString(i++));
+				u.setId(rs.getInt(i++));
+				u.setName(rs.getString(i++));
+				Integer idprod = rs.getInt(i++);
+				String nameprod = rs.getString(i++);
+				Integer idpubprod = rs.getInt(i++);
+				String namepubprod = rs.getString(i++);
+				if(idprod != null){
+					p.setId(idprod);
+					p.setName(nameprod);
+					n.setProduct(p);
+				}
+				else if(idpubprod != null){
+					pp.setId(idpubprod);
+					pp.setName(namepubprod);
+					n.setProduct(pp);
+				}
+				else
+					throw new DaoException("Notification without products");
+				
+				n.setUser(u);
+				n.setList(l);
+				list.add(n);
+			}
+			rs.close();
+			st.close();
+			return list;
+		}
+		catch(SQLException ex){
+			Logger.getLogger(UserDAOimpl.class.getName()).log(Level.SEVERE, null, ex);
+			throw new DaoException(ex);
+		}
+	}
+	
+	@Override
+	public List<Notification> getUnreadNotifications(Integer userId) throws DaoException {
+		throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+	}
+	
+	@Override
+	public Integer getNotificationCount(Integer userId) throws DaoException {
+		throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+	}
+	
+}
