f458387c335c568a1ae2b1e7629d68bc61325f7c
Giulia <giuliacarocari@gmail.com>
2018-08-29 15:22:39 +0200

    Rivisto filtro per la cancellazione

--- src/main/java/it/unitn/webprog2018/ueb/shoppinglist/filters/ListDeleteFilter.java
+++ src/main/java/it/unitn/webprog2018/ueb/shoppinglist/filters/ListDeleteFilter.java
@@ -12,6 +12,7 @@ import it.unitn.webprog2018.ueb.shoppinglist.entities.User;
 import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.regex.Pattern;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
@@ -20,6 +21,7 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  *
@@ -28,9 +30,11 @@ import javax.servlet.http.HttpServletResponse;
 public class ListDeleteFilter implements Filter {
 
 	private ListDAO listDAO;
-
+	private FilterConfig filterConfig;
+	
 	@Override
 	public void init(FilterConfig filterConfig) throws ServletException {
+		this.filterConfig = filterConfig;
 		listDAO = ((DAOFactory) filterConfig.getServletContext().getAttribute("daoFactory")).getListDAO();
 	}
 
@@ -38,20 +42,55 @@ public class ListDeleteFilter implements Filter {
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
 		if (request instanceof HttpServletRequest) {
 			HttpServletRequest req = (HttpServletRequest) request;
-			Integer currentListId = (Integer) request.getAttribute("currentListId");
-			try {
-				if (!listDAO.hasDeletePermission(currentListId, ((User) req.getSession().getAttribute("user")).getId())) {
-					((HttpServletResponse) response).sendError(401);
+			HttpSession session = ((HttpServletRequest) request).getSession(false);
+			User user = null;
+			if (session != null) {
+				user = (User) session.getAttribute("user");
+			}
+
+			if (user == null) { // Should never happen, but just in case
+				String contextPath = filterConfig.getServletContext().getContextPath();
+				if (!contextPath.endsWith("/")) {
+					contextPath += "/";
 				}
-			} catch (DaoException ex) {
-				((HttpServletResponse) response).sendError(500, "We could not determine if you have the right to delete this list");
+				request.getRequestDispatcher("/WEB-INF/views/auth/Login.jsp").forward(request, response);
+				return;
+			}
+
+			String uri = req.getRequestURI();
+			if (!uri.endsWith("/")) {
+				uri = uri.trim();
+				uri += "/";
+			}
+
+			if (Pattern.matches(".*/restricted/[a-zA-Z]+/" + user.getId() + "/.+", uri)) {
+				Integer listId;
+				if (uri.endsWith("/" + user.getId() + "/")) { // handles the case in which userId and listId have the same value
+					listId = user.getId();
+					request.setAttribute("currentListId", listId);
+				} else {
+					try {
+						listId = Integer.parseInt(uri.substring(uri.lastIndexOf("/", uri.length() - 2) + 1, uri.length() - 1));
+						request.setAttribute("currentListId", listId);
+					} catch (NumberFormatException ex) {
+						((HttpServletResponse) response).sendError(400, "You wrote this URL yourself, didn't you? This does not point to a list.");
+						return;
+					}
+				}
+				try {
+					if (!listDAO.hasDeletePermission(listId, ((User) req.getSession().getAttribute("user")).getId())) {
+						((HttpServletResponse) response).sendError(401);
+					}
+				} catch (DaoException ex) {
+					((HttpServletResponse) response).sendError(500, "We could not determine if you have the right to delete this list");
+					Logger.getLogger(ListDeleteFilter.class.getName()).log(Level.SEVERE, null, ex);
+				}
+			}
+			try {
+				chain.doFilter(request, response);
+			} catch (IOException | ServletException ex) {
 				Logger.getLogger(ListDeleteFilter.class.getName()).log(Level.SEVERE, null, ex);
 			}
-		}
-		try {
-			chain.doFilter(request, response);
-		} catch (IOException | ServletException ex) {
-			Logger.getLogger(ListDeleteFilter.class.getName()).log(Level.SEVERE, null, ex);
 		}
 	}
 
