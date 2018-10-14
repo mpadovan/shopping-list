8bf1c8b9b511b38ac621e8c0ab762d6da635a6ea
Giulia <giuliacarocari@gmail.com>
2018-10-14 17:46:28 +0200

    Add filtering on chat websocket

--- src/main/java/it/unitn/webprog2018/ueb/shoppinglist/websocket/chat/ChatSessionHandler.java
+++ src/main/java/it/unitn/webprog2018/ueb/shoppinglist/websocket/chat/ChatSessionHandler.java
@@ -7,9 +7,11 @@ package it.unitn.webprog2018.ueb.shoppinglist.websocket.chat;
 
 import com.google.gson.Gson;
 import it.unitn.webprog2018.ueb.shoppinglist.dao.exceptions.DaoException;
+import it.unitn.webprog2018.ueb.shoppinglist.dao.interfaces.ListDAO;
 import it.unitn.webprog2018.ueb.shoppinglist.entities.Message;
 import it.unitn.webprog2018.ueb.shoppinglist.entities.User;
 import it.unitn.webprog2018.ueb.shoppinglist.websocket.SessionHandler;
+import it.unitn.webprog2018.ueb.shoppinglist.ws.ListWebService;
 import java.io.IOException;
 import java.util.LinkedList;
 import java.util.List;
@@ -26,45 +28,56 @@ import javax.enterprise.context.ApplicationScoped;
  */
 @ApplicationScoped
 public class ChatSessionHandler extends SessionHandler {
-	
+
 	private static final Gson GSON = new Gson();
-	
+
 	public boolean persistMessage(Message message) throws DaoException {
-		getDaoFactory().getMessageDAO().addMessage(message);
-		return true;
+		if (checkViewPermission(message.getList().getId(), message.getSender().getId())) {
+			getDaoFactory().getMessageDAO().addMessage(message);
+			return true;
+		} else {
+			return false;
+		}
 	}
 
 	public List<Message> getMessages(Integer userId, Integer listId) throws DaoException {
-		User user = new User();
-		user.setId(userId);
-		it.unitn.webprog2018.ueb.shoppinglist.entities.List list = new it.unitn.webprog2018.ueb.shoppinglist.entities.List();
-		list.setId(listId);
-		return getDaoFactory().getMessageDAO().getLastMessages(list, user);
+		if (checkViewPermission(listId, userId)) {
+			User user = new User();
+			user.setId(userId);
+			it.unitn.webprog2018.ueb.shoppinglist.entities.List list = new it.unitn.webprog2018.ueb.shoppinglist.entities.List();
+			list.setId(listId);
+			return getDaoFactory().getMessageDAO().getLastMessages(list, user);
+		} else { 
+			return null;
+		}
 	}
-	
+
 	public List<ChatWebSocketUnreadCount> getUnreadCount(Integer userId) throws DaoException {
-		Map<Integer,Integer> map = getDaoFactory().getMessageDAO().getUnreadCount(userId);
+		Map<Integer, Integer> map = getDaoFactory().getMessageDAO().getUnreadCount(userId);
 		List<ChatWebSocketUnreadCount> ret = new LinkedList<>();
-		for (Entry<Integer,Integer> entry : map.entrySet()) {
+		for (Entry<Integer, Integer> entry : map.entrySet()) {
 			ret.add(new ChatWebSocketUnreadCount(entry.getKey(), entry.getValue()));
 		}
 		return ret;
 	}
 
 	void notifyNewMessage(Integer senderId, Integer listId) {
-		NotificationThread notificationThread = new NotificationThread(senderId, listId);
-		notificationThread.start();
+		if (checkViewPermission(listId, senderId)) {
+			NotificationThread notificationThread = new NotificationThread(senderId, listId);
+			notificationThread.start();
+		}
 	}
-	
+
 	private class NotificationThread extends Thread {
+
 		private final Integer senderId;
 		private final Integer listId;
-		
+
 		public NotificationThread(Integer senderId, Integer listId) {
 			this.senderId = senderId;
 			this.listId = listId;
 		}
-		
+
 		@Override
 		public void run() {
 			try {
@@ -82,6 +95,21 @@ public class ChatSessionHandler extends SessionHandler {
 			}
 
 		}
+	}
+
+	private boolean checkViewPermission(int listId, int userId) {
+		try {
+			ListDAO listDAO = SessionHandler.getDaoFactory().getListDAO();
+			it.unitn.webprog2018.ueb.shoppinglist.entities.List list = listDAO.getList(listId);
+
+			if (list.getOwner().getId().equals(userId)
+					|| listDAO.hasViewPermission(listId, userId)) {
+				return true;
+			}
+		} catch (DaoException ex) {
+			Logger.getLogger(ListWebService.class.getName()).log(Level.SEVERE, null, ex);
+		}
+		return false;
 	}
 
 }
